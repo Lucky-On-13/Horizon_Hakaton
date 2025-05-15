b5b095830da0f96f4e8c95aac0bda0f0aa3cbf46
NIANGA Claude Hussein <niangahussein93@gmail.com>
2025-05-15 12:28:18 +0100

    Update route.ts

--- app/api/dossiers/[id]/comments/route.ts
+++ app/api/dossiers/[id]/comments/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import prisma from '@/lib/prisma'
 
-export default async function POST(
+ async function POST(
   request: Request,
   { params }: { params: { id: string } }
 ) {
